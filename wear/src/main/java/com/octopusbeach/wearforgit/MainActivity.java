ad799574279762aed34550f9e01d83976ca8c43d
Hudson49423 <Hudsondeveloper@gmail.com>
2015-06-28 16:56:13 -0400

    Changed wear MainActivity to show a simple refresh button and a textview under to check for notifications

--- wear/src/main/java/com/octopusbeach/wearforgit/MainActivity.java
+++ wear/src/main/java/com/octopusbeach/wearforgit/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
